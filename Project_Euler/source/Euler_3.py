576e702107638a93f802137a93ee14cf9ee1b789
kiiiiiiiim <kim91818824@gmail.com>
2015-04-27 10:59:59 +0900

    first commit

---
+++ Project_Euler/source/Euler_3.py
@@ -0,0 +1,30 @@
+import time
+
+
+max = 142093402
+computed_max = max
+start = time.time()
+list = []
+######
+pos = 2
+while(pos <= computed_max):
+    for x in list:
+        if(pos % x == 0):
+            pos += 1
+            continue
+        3
+    if max % pos == 0:
+        list.append(pos)
+        sum = 1
+        
+        for x in list:
+            sum *= x
+            
+        computed_max = max - sum
+        print pos
+        
+    pos += 1
+    
+######
+end = time.time()
+print str("%.4f ms" % ((end - start) * 1000))
